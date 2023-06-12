3729461f90d7aca1f2bcde8e3308091457072f3b
ViniciusElias05 <vini-mq@hotmail.com>
2023-06-12 17:06:05 -0300

    Add welcome.stories

---
+++ src/stories/welcome.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { text } from "@storybook/addon-knobs";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toAutoEscolaAlmeida = () => (
+  <div>
+    <h1>Auto Escola Almeida</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic voluptates
+      tempore asperiores ab totam, voluptas voluptatum aliquam reprehenderit
+      illum quaerat nemo id quisquam perferendis consequuntur deserunt dicta
+      nostrum? Voluptate, asperiores?
+    </p>
+  </div>
+);
