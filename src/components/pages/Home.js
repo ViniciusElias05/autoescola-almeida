d1b739dfc05eee7ea13cf8fe22b9feae041859bb
ViniciusElias05 <vini-mq@hotmail.com>
2023-06-27 16:36:33 -0300

    Fix video auto play

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -84,7 +84,7 @@ const Home = () => (
           </div>
         </div>
         <div>
-          <video src={AboutVideo} width="100%" autoPlay loop muted />
+          <video src={AboutVideo} width="100%" autoPlay playsInline loop muted />
         </div>
       </Grid>
 
